c1941b82d191c4b3452fbd90b9829b5850dfac58
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-04-11 14:39:02 +0200

    Add a full_title helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-  get 'foo/baz'
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+
 end
